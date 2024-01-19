e1ab9f479dbbc081a8d2f10a326279f39ae280a9
Istiyak Ahmad <116272714+Istiyakahmad@users.noreply.github.com>
2024-01-19 23:46:00 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// This function clears all the values
-function clearScreen() {
-    document.getElementById("result").value = "";
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
- 
-// This function displays the values
-function display(value) {
-    document.getElementById("result").value += value;
-}
- 
-// This function evaluates the expression and returns the result
-function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-}
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+    
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+
